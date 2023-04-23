5003ff3a338e05fe2e9a3af12919c2a9b7ba78ca
Kuth <69514175+ezzhop@users.noreply.github.com>
2023-04-23 07:47:10 +0700

    Update button

--- static/js/sketchPad.js
+++ static/js/sketchPad.js
@@ -6,9 +6,17 @@ class SketchPad{
         this.canvas.style=`
         background-color:white;
         box-shadow: 0px 0px 10px 2px black;
-        border-radius: 10px;
+        border-radius: 5px;
         `;
         container.appendChild(this.canvas);
+
+        const lineBreak=document.createElement("br");
+        container.appendChild(lineBreak);
+
+        this.undoBtn=document.createElement("button");
+        this.undoBtn.innerHTML="UNDO";
+        container.appendChild(this.undoBtn);
+
         this.ctx=this.canvas.getContext("2d");
         this.paths = [];
         this.isDrawing=false;
@@ -48,6 +56,10 @@ class SketchPad{
         this.canvas.ontouchend=()=>{
             this.canvas.onmouseup();
         }
+        this.undoBtn.onclick=()=>{
+            this.paths.pop();
+            this.#redraw();
+        };
     }
     #redraw(){ 
         this.ctx.clearRect(0,0,
